02d2fa761aa0184fa35981005e965002efc5799c
barshaann <barshanghosh036@gmail.com>
2026-02-20 13:11:29 +0530

    Initial commit - Vote System project

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>
+    Copyright &copy; 2024 
+    <a href="https://barshaann.github.io/portfolio/" target="_blank">Barshan Ghosh</a>
+</strong>
+
+</footer>
