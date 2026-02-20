4b41aca3d1e47711a05ae182b17380d82104fc00
Barshan Ghosh <145197538+barshaann@users.noreply.github.com>
2026-02-20 23:04:09 +0530

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,8 +1,15 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
+
+$conn = new mysqli($host, $user, $password, $database, (int)$port);
+
+$conn->query("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
